eb29284866344d789d4f994b8ea14b5f6c0beae1
qiaotonghuang <huang.qiaot@northeastern.edu>
2022-09-21 14:10:57 +0800

    first commit

---
+++ src/model/User.java
@@ -0,0 +1,192 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Qiaotong
+ */
+public class User {
+    private String firstName;
+    private String lastName;
+    private String DOB;
+    private String age;
+    private String tel;
+    private String email;
+    private String streetLine_1;
+    private String streetLine_2;
+    private String city;
+    private String country;
+    private String affiliation;
+    private String major;
+    private String career;
+    private String degree_1;
+    private String degree_1_Start;
+    private String degree_1_End;
+    private String degree_2;
+    private String degree_2_Start;
+    private String degree_2_End;
+    private String photoAddress;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(String DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreetLine_1() {
+        return streetLine_1;
+    }
+
+    public void setStreetLine_1(String streetLine_1) {
+        this.streetLine_1 = streetLine_1;
+    }
+
+    public String getStreetLine_2() {
+        return streetLine_2;
+    }
+
+    public void setStreetLine_2(String streetLine_2) {
+        this.streetLine_2 = streetLine_2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAffiliation() {
+        return affiliation;
+    }
+
+    public void setAffiliation(String affiliation) {
+        this.affiliation = affiliation;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getCareer() {
+        return career;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+    public String getDegree_1() {
+        return degree_1;
+    }
+
+    public void setDegree_1(String degree_1) {
+        this.degree_1 = degree_1;
+    }
+
+    public String getDegree_1_Start() {
+        return degree_1_Start;
+    }
+
+    public void setDegree_1_Start(String degree_1_Start) {
+        this.degree_1_Start = degree_1_Start;
+    }
+
+    public String getDegree_1_End() {
+        return degree_1_End;
+    }
+
+    public void setDegree_1_End(String degree_1_End) {
+        this.degree_1_End = degree_1_End;
+    }
+
+    public String getDegree_2() {
+        return degree_2;
+    }
+
+    public void setDegree_2(String degree_2) {
+        this.degree_2 = degree_2;
+    }
+
+    public String getDegree_2_Start() {
+        return degree_2_Start;
+    }
+
+    public void setDegree_2_Start(String degree_2_Start) {
+        this.degree_2_Start = degree_2_Start;
+    }
+
+    public String getDegree_2_End() {
+        return degree_2_End;
+    }
+
+    public void setDegree_2_End(String degree_2_End) {
+        this.degree_2_End = degree_2_End;
+    }
+
+    public String getPhotoAddress() {
+        return photoAddress;
+    }
+
+    public void setPhotoAddress(String photoAddress) {
+        this.photoAddress = photoAddress;
+    }
+}
